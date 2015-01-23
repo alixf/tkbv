2c12d15b623d6bd531f3705e55628c5b86abc4e8
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-01-23 19:05:22 +0100

    Add test scene

---
+++ Assets/KnightMovement.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var speed = 1.0;
+
+function Start ()
+{
+
+}
+
+function Update ()
+{
+	var xStick = Input.GetAxis("Horizontal");
+	if(Mathf.Abs(xStick) > 0.5)
+		transform.position.x += (xStick - 0.5) * 2 * speed;
+}
