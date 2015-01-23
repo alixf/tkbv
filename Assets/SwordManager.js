a6d494ad45d3874e0cc13f03a02bc7540fbf5b19
unknown <orain.juliette@gmail.com>
2015-01-23 20:57:42 +0100

    Adding Sword collider activation on space down

---
+++ Assets/SwordManager.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	// TODO add "OR ||" with controller input
+	var actionBool = Input.GetKeyDown("space");
+	GetComponent(BoxCollider2D).enabled = actionBool;
+}
