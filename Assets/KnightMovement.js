9dca2c6b02678dbed14abf7daf1f64b94d1aa480
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-01-23 19:22:28 +0100

    Update knight's movement

--- Assets/KnightMovement.js
+++ Assets/KnightMovement.js
@@ -1,7 +1,8 @@
 ﻿#pragma strict
 
-var speed = 1.0;
-var deadzone = 0.5;
+var speed : float = 1.0;
+var deadzone : float = 0.5;
+var facing : int = 1;
 
 function Start ()
 {
@@ -16,6 +17,15 @@ function Update ()
     else
         stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
 
-    if(stickInput.x > 0.0)
+    if(stickInput.x != 0.0)
+    {
     	transform.position.x += stickInput.x * speed;
+    	if(stickInput.x < 0.0)
+    		facing = -1;
+    	else
+    		facing = 1;
+    }
+
+    if(transform.localScale.x * facing < 0.0)
+    	transform.localScale.x = -transform.localScale.x;
 }
