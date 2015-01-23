d8030c0c2c902a9308ae3fc65665bffb97d98054
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-01-23 19:09:31 +0100

    Update test scene

--- Assets/KnightMovement.js
+++ Assets/KnightMovement.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
 var speed = 1.0;
+var deadzone = 0.5;
 
 function Start ()
 {
@@ -9,7 +10,12 @@ function Start ()
 
 function Update ()
 {
-	var xStick = Input.GetAxis("Horizontal");
-	if(Mathf.Abs(xStick) > 0.5)
-		transform.position.x += (xStick - 0.5) * 2 * speed;
+	var stickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    if(stickInput.magnitude < deadzone)
+        stickInput = Vector2.zero;
+    else
+        stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
+
+    if(stickInput.x > 0.0)
+    	transform.position.x += stickInput.x * speed;
 }
