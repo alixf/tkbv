60b9f39d3c089a7143c968efda92b93b11531e11
unknown <orain.juliette@gmail.com>
2015-01-25 09:03:53 +0100

    Ajout des toppings et ramassage éléments

--- Assets/Scripts/GraalActions.js
+++ Assets/Scripts/GraalActions.js
@@ -1,14 +1,37 @@
 ﻿#pragma strict
 
 var graal:GameObject;
+var closeElement:String;
+var fill:int;
+var inside:String[];
+
 
 function Start () {
 	graal.GetComponentInChildren(Light).range = 0;
+	closeElement = "none";
+	fill=0;
+	inside = ["none","none"];
 }
 
 function Update () {
-
+	if(Input.GetKeyDown("space")) {
+		Debug.Log(inside);
+		// Rammasser. Max 2 éléments
+		if(closeElement != "none" && fill < 2) {
+			pickUp();
+		} else if (closeElement == "none") {
+		//	throwContent();
+		}
+	}
 }
+
+function pickUp (){
+	inside[fill++] = closeElement;
+}
+
+//function throwContent(){
+//	Instantiate(animations.inside[0], transform.position, transform.rotation);
+//}
 
 function OnTriggerEnter2D (collision : Collider2D) {
 	if(collision.gameObject.tag == "Element") {
@@ -16,9 +39,11 @@ function OnTriggerEnter2D (collision : Collider2D) {
 		Debug.Log(graal);
 		graal.GetComponentInChildren(Light).range = 0.5;
 		graal.GetComponentInChildren(Light).color = colorHalo;
+		closeElement = collision.gameObject.name; 
 	}
 }
 
 function OnTriggerExit2D(other : Collider2D) {
 		graal.GetComponentInChildren(Light).range = 0;
+		closeElement = "none";
 }
